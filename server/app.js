fc9b2f233e26987569312dfb11a7368dcea06061
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-03-13 16:32:49 +0000

    Remove test route from app

--- server/app.js
+++ server/app.js
@@ -9,10 +9,6 @@ const app = express();
 app.use(cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 }));
 app.use(express.json());
 
-app.get("/greet", (req, res) => {
-  res.status(200).json({ message: "Hello world!" });
-});
-
 app.use("/users", userRouter);
 
 module.exports = app;
