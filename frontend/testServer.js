193c76328b748e149188261ec92baffea83997c7
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-04-03 23:44:57 +0100

    Add database diagnostics to server test

--- frontend/testServer.js
+++ frontend/testServer.js
@@ -1,11 +1,14 @@
-const timeout = setTimeout(() => {
+console.log("===========================");
+console.log("RUNNING SERVER DIAGNOSTICS");
+
+const statusTimeout = setTimeout(() => {
   console.log("Timed out after 8 seconds");
   process.exit(-1);
 }, 8000);
 
 fetch("http://localhost:8000/status")
   .then((response) => {
-    clearTimeout(timeout);
+    clearTimeout(statusTimeout);
     if (response.ok) {
       return response.json();
     } else {
@@ -13,11 +16,48 @@ fetch("http://localhost:8000/status")
     }
   })
   .then((data) => {
-    console.log("SUCCESS");
+    console.log("========= SUCCESS =========");
+    console.log("     SERVER IS ONLINE");
     console.log(data);
   })
   .catch((error) => {
-    clearTimeout(timeout);
+    clearTimeout(statusTimeout);
+    console.log(error);
+    process.exit(-1);
+  });
+
+console.log("===========================");
+console.log("  RUNNING DB DIAGNOSTICS");
+
+const databaseTimeout = setTimeout(() => {
+  console.log("Timed out after 8 seconds");
+  process.exit(-1);
+}, 8000);
+
+fetch("http://localhost:8000/users", {
+  method: "post",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    username: "diagnosticUsername",
+    email: "diagnosticEmail@test.com",
+    password: "1234Password1234",
+  }),
+})
+  .then((response) => {
+    clearTimeout(databaseTimeout);
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`Status code: ${response.status} ${response.statusText}`);
+    }
+  })
+  .then((data) => {
+    console.log("========= SUCCESS =========");
+    console.log("    DATABASE IS ONLINE");
+    console.log(data);
+  })
+  .catch((error) => {
+    clearTimeout(databaseTimeout);
     console.log(error);
     process.exit(-1);
   });
