35cab7887022e47ed363a0016a8105d36444a7bf
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-04-21 16:41:08 +0100

    Write server err to file and display after tests

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs/promises");
 
 const User = require("./models/user");
 const userRouter = require("./routes/user");
@@ -14,6 +15,22 @@ app.use("/users", userRouter);
 app.use("/login", tokenRouter);
 app.get("/status", (req, res) => {
   res.status(200).json({ status: "online" });
+});
+app.get("/throw", (req, res) => {
+  throw new Error("Thrown from the GET /throw endpoint");
+  res.status(200).send("Success");
+});
+
+//Error handler that writes errors to file
+app.use(async (err, req, res, next) => {
+  console.log("EXPRESS ERROR HANDLER");
+  try {
+    await fs.writeFile("./server-error.log", err.stack);
+    next(err);
+  } catch (writeErr) {
+    console.log("Error in the writeFile section");
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = app;
