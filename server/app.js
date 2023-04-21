4c0e27c79ca9479a88ab1028a68ad44c20cd2aa8
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-04-21 18:10:31 +0100

    Remove utiliy paths from Express app

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs/promises");
 
-const User = require("./models/user");
 const userRouter = require("./routes/user");
 const tokenRouter = require("./routes/token");
 
@@ -15,22 +14,6 @@ app.use("/users", userRouter);
 app.use("/login", tokenRouter);
 app.get("/status", (req, res) => {
   res.status(200).json({ status: "online" });
-});
-app.get("/throw", (req, res) => {
-  throw new Error("Thrown from the GET /throw endpoint");
-  res.status(200).send("Success");
-});
-
-//Error handler that writes errors to file
-app.use(async (err, req, res, next) => {
-  console.log("EXPRESS ERROR HANDLER");
-  try {
-    await fs.writeFile("./server-error.log", err.stack);
-    next(err);
-  } catch (writeErr) {
-    console.log("Error in the writeFile section");
-    res.status(500).send("Server Error");
-  }
 });
 
 module.exports = app;
