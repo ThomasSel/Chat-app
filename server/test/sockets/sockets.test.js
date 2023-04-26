cc6e0cc112709eccec9bb8a26ff99ccdf1bc6395
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-04-26 14:53:46 +0100

    Echo messages back to client

--- server/test/sockets/sockets.test.js
+++ server/test/sockets/sockets.test.js
@@ -24,5 +24,29 @@ describe("Socket Server", () => {
       const client = await newClient();
       expect(client.readyState).toEqual(1);
     });
+
+    it("sends message back to the client", async () => {
+      const client = await newClient();
+      const messages = [];
+      client.on("message", (data) => {
+        messages.push(data.toString());
+        client.close();
+      });
+
+      client.send("test message");
+
+      await new Promise((resolve) => {
+        const interval = setInterval(() => {
+          if (client.readyState === 3) {
+            clearInterval(interval);
+            resolve();
+          }
+        }, 10);
+      });
+
+      expect(client.readyState === 3);
+      expect(messages.length).toEqual(1);
+      expect(messages[0]).toEqual("test message");
+    });
   });
 });
--- server/wsServer.js
+++ server/wsServer.js
@@ -2,4 +2,12 @@ const WebSocket = require("ws");
 
 const wsServer = new WebSocket.WebSocketServer({ noServer: true });
 
+wsServer.on("connection", (socket) => {
+  socket.on("error", console.error);
+  socket.on("close", () => {});
+  socket.on("message", (data) => {
+    socket.send(data.toString());
+  });
+});
+
 module.exports = wsServer;
