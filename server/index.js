57ed7acb7ee0beb1c5e59a5c56394459f955507d
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-04-26 18:00:44 +0100

    Attach websocket server to HTTP server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const http = require("http");
 const mongoose = require("mongoose");
 const app = require("./app");
+const wsServer = require("./wsServer");
 
 const mongoURL = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/chatApp";
 mongoose.connect(mongoURL);
@@ -12,6 +13,12 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const port = parseInt(process.env.PORT || "8000");
 const httpServer = http.createServer(app);
+
+httpServer.on("upgrade", (req, socket, head) => {
+  wsServer.handleUpgrade(req, socket, head, (socket) => {
+    wsServer.emit("connection", socket, req);
+  });
+});
 
 httpServer.listen(port, () => {
   console.log(`Listening to port ${port}`);
--- server/wsServer.js
+++ server/wsServer.js
@@ -5,6 +5,7 @@ const wsServer = new WebSocket.WebSocketServer({ noServer: true });
 wsServer.on("connection", (socket) => {
   socket.on("error", console.error);
   socket.on("close", () => {});
+
   socket.on("message", (data) => {
     wsServer.clients.forEach((client) => {
       client.send(data);
