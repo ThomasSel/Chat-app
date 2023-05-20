321b34940c411d4c49a6eb728fa80ba38c75caf0
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-05-20 14:39:21 +0100

    Add mount and signup type declarations

--- frontend/cypress/support/e2e.ts
+++ frontend/cypress/support/e2e.ts
@@ -1,23 +1,12 @@
-// ***********************************************************
-// This example support/e2e.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
 import "./commands";
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      signup(username: string, email: string, password: string): Chainable<any>;
+    }
+  }
+}
 
 Cypress.Commands.add(
   "signup",
