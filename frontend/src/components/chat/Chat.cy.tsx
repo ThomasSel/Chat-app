2d08b7949b8f9441f8f76aae2ec3304a17e8fa91
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-05-22 17:21:35 +0100

    Add tests for Chat component

---
+++ frontend/src/components/chat/Chat.cy.tsx
@@ -0,0 +1,28 @@
+import Chat from "./Chat";
+
+describe("Chat", () => {
+  it("displays the name of the chat", () => {
+    cy.mount(<Chat name="Test" messages={[]} />);
+
+    cy.get('[data-cy="chat-name"]').should("contain", "Test");
+  });
+
+  it("when messages are empty, displays nothing", () => {
+    cy.mount(<Chat name="Test" messages={[]} />);
+
+    cy.get('[data-cy="chat-messages"]').should("not.contain.text");
+  });
+
+  it("displays all messages given as props", () => {
+    cy.mount(<Chat name="Test" messages={["message 1", "message 2"]} />);
+
+    cy.get('[data-cy="chat-messages"]').should(
+      "contain.html",
+      "<li>message 1</li>"
+    );
+    cy.get('[data-cy="chat-messages"]').should(
+      "contain.html",
+      "<li>message 2</li>"
+    );
+  });
+});
--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -6,14 +6,13 @@ type ChatProps = {
 const Chat = ({ name, messages }: ChatProps): JSX.Element => {
   return (
     <div>
-      <h1>{name}</h1>
-      <div>
-        <ul>
-          {messages.map((message) => (
-            <li>{message}</li>
-          ))}
-        </ul>
-      </div>
+      <h1 data-cy="chat-name">{name}</h1>
+
+      <ul data-cy="chat-messages">
+        {messages.map((message) => (
+          <li>{message}</li>
+        ))}
+      </ul>
     </div>
   );
 };
