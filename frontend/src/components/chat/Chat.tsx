8e766744e42331764580339b9ac9608e21873eea
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-05-25 13:59:50 +0100

    Add Chat messages styling

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./Chat.css";
 
 type ChatProps = {
@@ -9,6 +9,11 @@ type ChatProps = {
 
 const Chat = ({ name, messages, socket }: ChatProps): JSX.Element => {
   const [message, setMessage] = useState<string>("");
+  const messagesRef = useRef<HTMLUListElement | null>(null);
+
+  useEffect(() => {
+    messagesRef?.current.scrollTo({ top: Number.MAX_SAFE_INTEGER });
+  }, [messages]);
 
   const handleSubmit: React.FormEventHandler = (e) => {
     e.preventDefault();
@@ -20,31 +25,29 @@ const Chat = ({ name, messages, socket }: ChatProps): JSX.Element => {
     <div className="current-chat">
       <h1 data-cy="chat-name">{name}</h1>
 
-      <div className="chat-wrapper">
-        <ul data-cy="chat-messages" className="chat-messages">
-          {messages.map((message) => (
-            <li key={message} className="chat-message">
-              {message}
-            </li>
-          ))}
-        </ul>
-        <form onSubmit={handleSubmit} className="message-form">
-          <input
-            type="text"
-            data-cy="chat-input"
-            onChange={(e) => setMessage(e.target.value)}
-            value={message}
-            placeholder={`Send a message to #${name}`}
-            className="message-input"
-          />
-          <input
-            type="submit"
-            value="Send"
-            data-cy="chat-submit"
-            className="message-send"
-          />
-        </form>
-      </div>
+      <ul data-cy="chat-messages" className="chat-messages" ref={messagesRef}>
+        {messages.map((message) => (
+          <li key={message} className="chat-message">
+            {message}
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit} className="message-form">
+        <input
+          type="text"
+          data-cy="chat-input"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          placeholder={`Send a message to #${name}`}
+          className="message-input"
+        />
+        <input
+          type="submit"
+          value="Send"
+          data-cy="chat-submit"
+          className="message-send"
+        />
+      </form>
     </div>
   );
 };
