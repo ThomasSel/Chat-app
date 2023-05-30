f32123b0f04a59e6b08e7da0a6a68e90960b669b
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-05-30 17:28:12 +0100

    Add basic auth to socket server

--- server/test/sockets/auth.test.ts
+++ server/test/sockets/auth.test.ts
@@ -44,11 +44,7 @@ describe("Socket Auth", () => {
     it("doesn't close the connection if the auth token is valid", async () => {
       const client = new wsClient(wsAddress);
 
-      await client.send(
-        JSON.stringify({
-          token: token,
-        })
-      );
+      await client.send(JSON.stringify({ token: token }));
 
       expect(client.readyState).toEqual(1);
     });
@@ -56,15 +52,32 @@ describe("Socket Auth", () => {
     it("doesn't close the connection on subsequent messages", async () => {
       const client = new wsClient(wsAddress);
 
-      await client.send(
-        JSON.stringify({
-          token: token,
-        })
+      await client.send(JSON.stringify({ token: token }));
+
+      const recieved = client.expectMessages(1);
+      await client.send("test message");
+      await recieved;
+
+      expect(client.messages.length).toEqual(1);
+    });
+  });
+
+  describe("invalid authentication message", () => {
+    it("closes the connection with invalid signature", async () => {
+      const token = jwt.sign(
+        {
+          userId: "12345678",
+          username: "fakeUsername",
+          iat: Math.floor(Date.now() / 1000),
+        },
+        "incorrectSecret"
       );
 
-      await client.send("Hello world");
+      const client = new wsClient(wsAddress);
+      await client.send(JSON.stringify({ token: token }));
 
-      expect(client.readyState).toEqual(1);
+      await client.waitClosed();
+      expect(client.readyState).toBeGreaterThan(1);
     });
   });
 });
--- server/wsServer.ts
+++ server/wsServer.ts
@@ -1,16 +1,42 @@
-import WebSocket from "ws";
+import ws from "ws";
+import jwt from "jsonwebtoken";
 
-const wsServer = new WebSocket.WebSocketServer({ noServer: true });
+const wsServer = new ws.WebSocketServer({ noServer: true });
 
 wsServer.on("connection", (socket) => {
   socket.on("error", console.error);
   socket.on("close", () => {});
 
-  socket.on("message", (data) => {
-    wsServer.clients.forEach((client) => {
-      client.send(data);
-    });
-  });
+  socket.onmessage = async (e: ws.MessageEvent) => {
+    try {
+      const data = JSON.parse(e.data.toString());
+      const payload = await new Promise<jwt.JwtPayload>((resolve, reject) => {
+        if (process.env.JWT_SECRET === undefined) {
+          return reject(new Error("Missing JWT_SECRET environment variable"));
+        }
+
+        jwt.verify(
+          data.token,
+          process.env.JWT_SECRET,
+          (error: any, payload: any) => {
+            if (error) {
+              return reject(error);
+            }
+            resolve(payload);
+          }
+        );
+      });
+    } catch (err) {
+      console.error(err);
+      socket.close();
+    }
+
+    socket.onmessage = (newE: ws.MessageEvent) => {
+      wsServer.clients.forEach((client) => {
+        client.send(newE.data.toString());
+      });
+    };
+  };
 });
 
 export default wsServer;
