bb056537af265cd9ea02eed10b4ff9e3bca9932b
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-06-03 12:03:17 +0100

    Add password hashing to create user controller

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,15 +1,20 @@
-import User from "../models/user";
 import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+
+import User from "../models/user";
 
 export const create = async (req: Request, res: Response): Promise<void> => {
   const email: string = req.body.email;
   const username: string = req.body.username;
   const password: string = req.body.password;
 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   const user = new User({
     email: email,
     username: username,
-    password: password,
+    password: hashedPassword,
   });
 
   try {
